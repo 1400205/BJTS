39c90250e76bebe1c3dd2e0536cb06e848327913
1400205 <1400205@rgu.ac.uk>
2016-03-02 16:43:55 +0000

    home.php

--- home.php
+++ home.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: prosper
- * Date: 02/03/2016
- * Time: 16:39
- */
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello</h1>
+</body>
+</html>
