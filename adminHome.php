f20d8f6e1af1252f6cfa3aa896fe3b4920d40769
1400205 <1400205@rgu.ac.uk>
2016-03-07 23:12:14 +0000

    added user home and admin home

---
+++ adminHome.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+<h1>Hello</h1>
+
+<?php
+// Echo session variables that were set on previous page
+echo "user name: " . $_SESSION["uname"] . ".<br>";
+echo "password: " . $_SESSION["pwd"] . ".";
+
+echo  $_SESSION["usertype"]. "<br>";
+echo $_SESSION["userstatus"]."<br>";
+echo $_SESSION["userid"]."<br>";
+
+?>
+</body>
+</html>
