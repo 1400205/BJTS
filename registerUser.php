33546417fc3ca79bb754dd77705520569f871422
1400205 <1400205@rgu.ac.uk>
2016-03-07 23:26:57 +0000

    user registeration form

---
+++ registerUser.php
@@ -0,0 +1,50 @@
+
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 07/03/2016
+ * Time: 23:25
+ */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug and Job Tracking System</title>
+</head>
+<body>
+
+<header>
+
+    <h3>Bug & Job Tracking System</h3>
+</header>
+
+<section>
+    <form>
+        <h4>Create Account</h4>
+        <lable for="username">Your Name</lable><br/>
+        <input type="text" name="username"/>
+        <p></p>
+        <lable for="Email">Email</lable><br/>
+        <input type="email" name="Email"/>
+        <p></p>
+
+        <lable for="Email">Email Again</lable><br/>
+        <input type="email" name="Email"/>
+        <p></p>
+        <lable for="phone">Your Phone Number</lable><br/>
+        <input type="tel" name="phone"/>
+        <p></p>
+        <lable for="password">Your Password</lable><br/>
+        <input type="password" name="password"/>
+        <p></p>
+        <lable for="password">Your Password Again</lable><br/>
+        <input type="password" name="password"/>
+        <p></p>
+
+        <input type="submit" Value="Create your BJTS Account"/>
+
+    </form>
+</section>
+
+</body>
+</html>
