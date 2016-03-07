c8ea28ae34601f331206f9346721ba8066a934c6
1400205 <1400205@rgu.ac.uk>
2016-03-07 22:46:49 +0000

    added user home and admin home

--- login.php
+++ login.php
@@ -22,25 +22,26 @@ if(empty($_POST["username"]) || empty($_POST["password"]))
 
 $sql="SELECT uid,userType,userStatus FROM users WHERE username='$username' and password='$password'";
 $result=mysqli_query($db,$sql);
-$row=mysqli_fetch_array($result);
 
-if(mysqli_num_rows($result) == 1)
+$row=mysqli_fetch_array($result);//get the row of data
 
+if(mysqli_num_rows($result) == 1 and $usertype=='Admin' and $userstatus==1) {
 
-{
-
-    $usertype=$row['userType'];//get user type
-    $userstatus=$row['userStatus'];//get user status
-    header("location: home.php"); // Redirecting To another Page
+    $usertype = $row['userType'];//get user type
+    $userstatus = $row['userStatus'];//get user status
+    header("location: adminHome.php"); // Redirecting To another Page
 
     $_SESSION["uname"] = $username;
     $_SESSION["pwd"] = $password;
 
-    $_SESSION["usertype"] =  $usertype;
-    $_SESSION["userstatus"] =  $userstatus;
-
-
-}else
+    $_SESSION["usertype"] = $usertype;
+    $_SESSION["userstatus"] = $userstatus;
+}
+elseif (mysqli_num_rows($result) == 1 and $usertype=='Non-Admin' and $userstatus==1)
+    {
+        header("location: userHome.php"); // Redirecting To another Page
+    }
+else
 {
     echo "Incorrect username or password.";
 
