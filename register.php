5f0e60126406edf231e1ffb434f1044d72ef4f05
1400205 <1400205@rgu.ac.uk>
2016-03-08 01:02:32 +0000

    test insert query

--- register.php
+++ register.php
@@ -9,40 +9,34 @@
 //call connection string fucntion
 include ("connect.php");//Establishing connection with our database
 
-//check for emptyness of user inputs
+//check connection
+if ($db==false)
+{die("could not connect");}
 
-if(empty($_POST["username"]) || empty($_POST["password"]) || empty($_POST["email"])|| empty($_POST["phone"]) )
-{
-    echo "Both fields are required.";
-}else {
-
-    //get user input data from form into variables
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-}
+//attempt excuting query
 
 //Build a query string to insert data into users table
-$qry="INSERT  INTO users(username,password,emailAddress,phoneExtention) VALUES '$username','$password','$email','$phone'";
+$qry="INSERT  INTO users(username,password,emailAddress,phoneExtention) VALUES 'kanda','pass','kandagh2000@gmail.com','0208233480'";
+
+if(mysqli_query($db, $sql)){
+    echo "Records added successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+}
+
+// Close connection
+mysqli_close(db);
+
+    //get user input data from form into variables
+    //$username = $_POST['username'];
+    //$password = $_POST['password'];
+    //$email = $_POST['email'];
+   // $phone = $_POST['phone'];
+
+//Build a query string to insert data into users table
+//$qry="INSERT  INTO users(username,password,emailAddress,phoneExtention) VALUES '$username','$password','$email','$phone'";
 
 //assign query string and connection variables to sqli guiry function
 
-$result= mysqli_query($db,$sql);
-
-mysqli_select_db('BJTS');
-
-
-//test connection
-if(! $db){
-    die("could not connect to database");
-}elseif($result)
-{
-die("could not enter data");
-}else
-{
-    echo "Entered data successfully\n";
-    mysqli_close($db);
-}
 
 
