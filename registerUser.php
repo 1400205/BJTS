56db2b5a466a4071a910567967eb71f14cf5791b
1400205 <1400205@rgu.ac.uk>
2016-03-08 01:33:39 +0000

    update with user registration

--- registerUser.php
+++ registerUser.php
@@ -20,17 +20,17 @@
 </header>
 
 <section>
-    <form method="post" action="register.php">
+    <form method="post" action="userRegistration.php.php">
         <h3>Create Account</h3>
         <lable for="username">User Name</lable><br/>
         <input type="text" name="username"/>
         <p></p>
         <lable for="Email">Email</lable><br/>
-        <input type="email" name="Email"/>
+        <input type="email" name="email"/>
         <p></p>
 
         <lable for="Email">Email Again</lable><br/>
-        <input type="email" name="Email"/>
+        <input type="email" name="Email1"/>
         <p></p>
         <lable for="phone">Your Phone Number</lable><br/>
         <input type="tel" name="phone"/>
@@ -39,7 +39,7 @@
         <input type="password" name="password"/>
         <p></p>
         <lable for="password">Your Password Again</lable><br/>
-        <input type="password" name="password"/>
+        <input type="password" name="password1"/>
         <p></p>
 
         <input type="submit" Value="Create your BJTS Account"/>
