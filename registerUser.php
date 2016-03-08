f04406a07d7276ff456304e4f1b7cd87213180c2
1400205 <1400205@rgu.ac.uk>
2016-03-08 01:42:19 +0000

    post with same page and eliminate variable page

--- registerUser.php
+++ registerUser.php
@@ -6,6 +6,65 @@
  * Time: 23:25
  */
 ?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 07/03/2016
+ * Time: 23:36
+ */
+
+//call connection string fucntion
+include ("connect.php");//Establishing connection with our database
+
+
+//check emptyness of input boxies
+
+if(empty($_POST["username"]) || empty($_POST["password"])|| empty($_POST["email"])|| empty($_POST["phone"]))
+{
+    echo "All fields are required.";
+}else {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+
+    //check connection
+    if ($db==false)
+    {die("could not connect");}
+
+//attempt excuting query
+
+//Build a query string to insert data into users table
+    $qry="INSERT  INTO users(username, password, emailAddress, phoneExtention) VALUES ('$username', '$password', '$email', '$phone')";
+
+    if(mysqli_query($db, $qry)){
+        echo "Records added successfully.";
+    } else{
+        echo "ERROR: Could not able to execute $qry. " . mysqli_error($db);
+    }
+
+// Close connection
+    mysqli_close(db);
+
+//get user input data from form into variables
+//$username = $_POST['username'];
+//$password = $_POST['password'];
+//$email = $_POST['email'];
+// $phone = $_POST['phone'];
+
+//Build a query string to insert data into users table
+//$qry="INSERT  INTO users(username,password,emailAddress,phoneExtention) VALUES '$username','$password','$email','$phone'";
+
+//assign query string and connection variables to sqli guiry function
+
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,7 +79,7 @@
 </header>
 
 <section>
-    <form method="post" action="userRegistration.php">
+    <form method="post" action="registerUser.php">
         <h3>Create Account</h3>
         <lable for="username">User Name</lable><br/>
         <input type="text" name="username"/>
