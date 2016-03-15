398e8882e802a82748e207f78007b21b6313e9b3
1400205 <1400205@rgu.ac.uk>
2016-03-15 02:26:39 +0000

    direct new users to login page

---
+++ inactiveUsers.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 15/03/2016
+ * Time: 02:17
+ */
+include ("connect.php");
+
+$dg = new C_DataGrid("SELECT * FROM users","uid","users");
+$sg->display();
