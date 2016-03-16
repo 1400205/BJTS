e7e490a2929e21edb68eac0fc9dd2597bef0abed
1400205 <1400205@rgu.ac.uk>
2016-03-16 16:53:40 +0000

    added table

--- inactive.php
+++ inactive.php
@@ -29,6 +29,6 @@ WHILE($row=mysqli_fetch_assoc($result))
     $userStatus=$row['userStatus'];
     $username=$row['username'];
 
-    echo '<table>'. '<tr>'.'<a href="inactive.php?uid="'.$uid.'>'.'<tr>'.'<td>'.$uid.'</td>'.'<td>' .$username.'</td>'.'<td>'. $userType.'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+    echo '<table style="width:100%">'. '<tr>'.'<a href="inactive.php?uid="'.$uid.'>'.'<tr>'.'<td>'.$uid.'</td>'.'<td>' .$username.'</td>'.'<td>'. $userType.'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
 }
 ?>
