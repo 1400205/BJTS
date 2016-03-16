ba083b9118b4c873d81b3b1e490a1efd8cfb9617
1400205 <1400205@rgu.ac.uk>
2016-03-16 05:26:55 +0000

    data grid

--- test.php
+++ test.php
@@ -6,3 +6,7 @@
  * Time: 04:22
  */
 include ("connect.php");//Establishing connection with our database
+
+$dg = new C_DataGrid("SELECT * FROM users","uid","users");
+//$result=mysqli_query($db,$dg);
+$dg->display();
