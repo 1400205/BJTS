64e7ba5f74802f111fb0f2a66cfde1ebc53f1c31
1400205 <1400205@rgu.ac.uk>
2016-03-16 14:55:30 +0000

    assign connection

--- login.php
+++ login.php
@@ -42,7 +42,7 @@ if(mysqli_num_rows($result) == 1 and $usertype=='Admin' and $userstatus==1) {
     $_SESSION["userstatus"] = $userstatus;//user stautus assigned to session global variable
     $_SESSION["userid"] = $userid;//user id assigned to session global variable
 }
-elseif (mysqli_num_rows($result) == 1 and $usertype=='Non-Admin' and $userstatus==1)
+elseif (mysqli_num_rows($result) == 1 and $usertype=='Non-Admin' and $userstatus==0)
     {
         header("location: userHome.php"); // Redirecting To another Page
     }
