df0a41cf3c455e8426e85e67b8b22f38e0469e2d
1400205 <1400205@rgu.ac.uk>
2016-03-16 18:01:43 +0000

    added input button for activation of users

--- inactive.php
+++ inactive.php
@@ -22,7 +22,8 @@ $sql="SELECT uid,userType,userStatus,username FROM users WHERE userStatus='0'";/
 $result=mysqli_query($db,$sql);//fetch data from database
 
 echo '<h3>In active Users Pending Activation</h3>';
-echo '<table border="1" style="width:60%">'.'<col width="75">'.'<col width="75">'.'<col width="75">'.'<th>'.'User ID'.'</th>'.'<th>'.'User Name'.'</th>'.'<th>'.'User Status'.
+echo '<table border="1" style="width:60%">'.'<col width="75">'.'<col width="75">'.'<col width="75">'.'<th>'.'User ID'.
+    '</th>'.'<th>'.'User Name'.'</th>'.'<th>'.'User Status'.
 
     '</th>'.'</table>';
 //loop through the database and fetch all users with userStatus=0
@@ -36,6 +37,6 @@ WHILE($row=mysqli_fetch_assoc($result))
 
     echo '<table border="1" style="width:60%">'.'<col width="75">'. '<col width="75">'.'<col width="75">'.'<tr>'.
         '<a href="inactive.php?uid="'.$uid.'>'.'<tr>'.'<td>'.$uid.'</td>'.'<td>' .$username.'</td>'.'<td>'.
-        $userType.'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+        $userType.'</td>'.'<td>'.'<'.'form'.'>'. 'input type="submit" name="submit" value = "activate"'.'<'.'/'.'form'.'>'.'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
 }
 ?>
