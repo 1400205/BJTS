f855649e635d5c8c2a6c5d5ed8666f7dfd70e526
1400205 <1400205@rgu.ac.uk>
2016-03-16 06:05:48 +0000

    assign connection

--- test.php
+++ test.php
@@ -5,8 +5,8 @@
  * Date: 16/03/2016
  * Time: 04:22
  */
-//include ("connect.php");//Establishing connection with our database
+include ("connect.php");//Establishing connection with our database
 
-//$dg = new C_DataGrid("SELECT * FROM users","uid","users");
-//$result=mysqli_query($db,$dg);
-//$dg->display();
+$dg = new C_DataGrid("SELECT * FROM users","uid","users");
+$result=mysqli_query($db,$dg);
+$result->display();
