c59528134b344c5ebbb5764a88f613f888bde62c
1400205 <1400205@rgu.ac.uk>
2016-03-17 22:06:23 +0000

    registration form modified

--- registerUser.php
+++ registerUser.php
@@ -7,64 +7,6 @@
  */
 ?>
 
-<?php
-/**
- * Created by PhpStorm.
- * User: prosper
- * Date: 07/03/2016
- * Time: 23:36
- */
-
-//call connection string fucntion
-include ("connect.php");//Establishing connection with our database
-
-
-//check emptyness of input boxies
-
-if(empty($_POST["username"]) || empty($_POST["password"])|| empty($_POST["email"])|| empty($_POST["phone"]))
-{
-    echo "All fields are required.";
-}else {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $email=$_POST['email'];
-    $phone=$_POST['phone'];
-
-    //check connection
-    if ($db==false)
-    {die("could not connect");}
-
-//attempt excuting query
-
-//Build a query string to insert data into users table
-    $qry="INSERT  INTO users(username, password, emailAddress, phoneExtention) VALUES ('$username', '$password', '$email', '$phone')";
-
-    if(mysqli_query($db, $qry)){
-        echo "Records added successfully.";
-    } else{
-        echo "ERROR: Could not able to execute $qry. " . mysqli_error($db);
-    }
-
-// Close connection
-    mysqli_close(db);
-
-//get user input data from form into variables
-//$username = $_POST['username'];
-//$password = $_POST['password'];
-//$email = $_POST['email'];
-// $phone = $_POST['phone'];
-
-//Build a query string to insert data into users table
-//$qry="INSERT  INTO users(username,password,emailAddress,phoneExtention) VALUES '$username','$password','$email','$phone'";
-
-//assign query string and connection variables to sqli guiry function
-
-}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -79,6 +21,7 @@ if(empty($_POST["username"]) || empty($_POST["password"])|| empty($_POST["email"
 </header>
 
 <section>
+    <fieldset>
     <form method="post" action="userRegistration.php">
         <h3>Create Account</h3>
         <lable for="username">User Name</lable><br/>
@@ -104,6 +47,7 @@ if(empty($_POST["username"]) || empty($_POST["password"])|| empty($_POST["email"
         <input type="submit" Value="Create your BJTS Account"/>
 
     </form>
+    </fieldset>
 </section>
 
 </body>
