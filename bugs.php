eadf2d8786a356397b5a0ad263180f01ca6114a6
1400205 <1400205@rgu.ac.uk>
2016-03-18 02:47:41 +0000

    Using PHP form and Post for input

--- bugs.php
+++ bugs.php
@@ -1,44 +1,33 @@
+
 <?php
-//start session
+//open start session
 
 session_start();
+?>
 ?>
 <?php
 /**
  * Created by PhpStorm.
  * User: prosper
- * Date: 17/03/2016
- * Time: 22:35
+ * Date: 07/03/2016
+ * Time: 23:36
  */
-//get connection string
+
+//call connection string fucntion
 include ("connect.php");//Establishing connection with our database
-{
+
+
 //check emptyness of input boxies
 
-if(empty($_POST["title"]) || empty($_POST["description"]))
+if(empty($_POST["username"]) || empty($_POST["title"])|| empty($_POST["description"]))
 {
     echo "All fields are required.";
-
-}
-else
-
-{
-    $uid=$_SESSION["userid"];
-    $postdate= new DateTime('Y-m-d H:i:s');//get current date
+}else {
     $title = $_POST['title'];
-    $mydesc = $_POST['description'];
+    $description = $_POST['description'];
+   $uid=$_SESSION['email'];
+   // $phone=$_POST['phone'];
 
-    //get bug tittles
-    //$sql="SELECT title FROM bugs WHERE  title='$title'";//select required dataset from database
-   // $result=mysqli_query($db,$sql);//fetch data from database
-   // $row=mysqli_fetch_array($result);//get the row of data
-
-   // if(mysqli_num_rows($result) == 1)
-   // {
-    //    echo "Bug with the title ". $tittle. " already exists";
-    //    echo "Duplicate bugs can not be submitted";
-    //
-    }
     //check connection
     if ($db==false)
     {die("could not connect");}
@@ -46,23 +35,30 @@ else
 //attempt excuting query
 
 //Build a query string to insert data into users table
-    $qry="INSERT  INTO bugs(title,bugDesc,uid) VALUES ('$title', '$mydesc', $uid')";
+    $qry="INSERT  INTO bugs(title, bugDesc, uid) VALUES ('$title',$description', '$uid')";
 
     if(mysqli_query($db, $qry)){
-       echo "Records added successfully.";
+        $_SESSION['success']= "Records added successfully.";
 
         //redirect user to login screen
-       // header("location: index.php");
+        header("location: index.php");
     } else{
         echo "ERROR: Could not be able to execute"/**$qry. mysqli_error($db)*/;
     }
 
+// Close connection
+    mysqli_close(db);
 
+//get user input data from form into variables
+//$username = $_POST['username'];
+//$password = $_POST['password'];
+//$email = $_POST['email'];
+// $phone = $_POST['phone'];
 
+//Build a query string to insert data into users table
+//$qry="INSERT  INTO users(username,password,emailAddress,phoneExtention) VALUES '$username','$password','$email','$phone'";
 
-
-
-
+//assign query string and connection variables to sqli guiry function
 
 }
 
