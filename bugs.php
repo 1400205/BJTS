1f17041fb6bccdf24cd86b95ee90878928319830
1400205 <1400205@rgu.ac.uk>
2016-03-18 01:15:11 +0000

    Using PHP form and Post for input

--- bugs.php
+++ bugs.php
@@ -20,31 +20,25 @@ if(empty($_POST["title"]) || empty($_POST["description"]))
     echo "All fields are required.";
 
 }
-elseif(isset($_POST['submit']))
+else
 
 {
     $uid=$_SESSION["userid"];
     $postdate= new DateTime();//get current date
     $title = $_POST['title'];
-    $desc = $_POST['description'];
+    $mydesc = $_POST['description'];
 
     //get bug tittles
+    //$sql="SELECT title FROM bugs WHERE  title='$title'";//select required dataset from database
+   // $result=mysqli_query($db,$sql);//fetch data from database
+   // $row=mysqli_fetch_array($result);//get the row of data
 
-    $sql="SELECT title FROM bugs WHERE  title='$title'";//select required dataset from database
-    $result=mysqli_query($db,$sql);//fetch data from database
-    $row=mysqli_fetch_array($result);//get the row of data
-
-    if(mysqli_num_rows($result) == 1)
-    {
-        echo "Bug with the title ". $tittle. " already exists";
-        echo "Duplicate bugs can not be submitted";
+   // if(mysqli_num_rows($result) == 1)
+   // {
+    //    echo "Bug with the title ". $tittle. " already exists";
+    //    echo "Duplicate bugs can not be submitted";
+    //
     }
-
-
-}
-
-
-else {
     //check connection
     if ($db==false)
     {die("could not connect");}
@@ -52,7 +46,7 @@ else {
 //attempt excuting query
 
 //Build a query string to insert data into users table
-    $qry="INSERT  INTO bugs(title, bugDesc, postDate,uid) VALUES ('$title', '$desc', '$postDate',$uid)";
+    $qry="INSERT  INTO bugs(title, bugDesc, postDate,uid) VALUES ('$title', '$mydesc', '$postDate',$uid)";
 
     if(mysqli_query($db, $qry)){
        echo "Records added successfully.";
