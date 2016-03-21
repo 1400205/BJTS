1e9085b6a4a71265268e27bd6d58bf385ad003b7
1400205 <1400205@rgu.ac.uk>
2016-03-21 15:45:41 +0000

    strip variable from all sql inections

---
+++ bug.php
@@ -0,0 +1,33 @@
+
+<?php
+//open start session
+
+session_start();
+?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 21/03/2016
+ * Time: 15:39
+ */
+
+//call connection string fucntion
+include ("connect.php");//Establishing connection with our database
+
+if(isset($_POST["submit"])) {
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+    $uid = $_SESSION["userid"];
+
+//strip variables of all sql injections
+    $title = mysqli_real_escape_string($db, $title);
+    $description = mysqli_real_escape_string($db, $description);
+    $uid = mysqli_real_escape_string($db, $uid);
+
+    $sql = "SELECT title FROM bugs WHERE title='$title'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result);//get the row of data
+    echo $row["title"];
+}
