4f798d4441e7795c76512dd151fae626c5cfdf0b
1400205 <1400205@rgu.ac.uk>
2016-03-21 14:13:45 +0000

    strip variable from all sql inections

--- bugs.php
+++ bugs.php
@@ -35,15 +35,17 @@ elseif(isset($_POST['submit']))
     $description=mysqli_real_escape_string($db,$description);
     $uid=mysqli_real_escape_string($db,$uid);
 
-    $gettitle="SELECT title FROM bugs WHERE title='$title'";
-    $result=mysqli_query($db,$gettitle);
-
+    $sql="SELECT title FROM bugs WHERE title='$title'";
+    $result=mysqli_query($db,$sql);
     $row=mysqli_fetch_array($result);//get the row of data
-   // $row=mysqli_fetch_array($gettitle,mysqli_fetch_assoc);
+
 
 
     if (mysqli_num_rows($result)==1)
     {
+
+        header("location: bugRegister.php"); // Redirecting To another Page
+
         $msg="Sorry This Bug already exists";
         print '$msg';
     }else
