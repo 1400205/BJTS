d9ce614c6609bd849093e0a5b0692290ab9e159b
1400205 <1400205@rgu.ac.uk>
2016-03-21 17:35:41 +0000

    strip variable from all sql inections

--- registerUser.php
+++ registerUser.php
@@ -31,17 +31,12 @@
         <input type="email" name="email"/>
         <p></p>
 
-        <lable for="Email">Email Again</lable><br/>
-        <input type="email" name="Email1"/>
-        <p></p>
         <lable for="phone">Your Phone Number</lable><br/>
         <input type="tel" name="phone"/>
         <p></p>
         <lable for="password">Your Password</lable><br/>
         <input type="password" name="password"/>
-        <p></p>
-        <lable for="password">Your Password Again</lable><br/>
-        <input type="password" name="password1"/>
+
         <p></p>
 
         <input type="submit" Value="Create your BJTS Account"/>
