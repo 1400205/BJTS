f51aacaf7d93fad315c454e712a44188602c0933
1400205 <1400205@rgu.ac.uk>
2016-03-21 17:50:00 +0000

    strip variable from all sql inections

---
+++ Registrations.php
@@ -0,0 +1,29 @@
+
+<?php
+//open start session
+
+session_start();
+?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 21/03/2016
+ * Time: 17:43
+ */
+//get connection string
+include("connect.php");
+
+//check if form is fill, collect form variables
+if( $_POST["submit"])
+{
+    //collect form variables
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+
+    echo "$email";
+}
