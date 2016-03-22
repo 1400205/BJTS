90825e9e5bfbe9edbb684ede82823caba7c73fcf
1400205 <1400205@rgu.ac.uk>
2016-03-22 06:02:59 +0000

    html home page

--- changeBugStatus.php
+++ changeBugStatus.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Change Bug Status</title>
+    <title>Inactive users</title>
 </head>
 <body>
 
@@ -19,34 +19,27 @@ session_start();
  *
  */
 
-//get user ID in session
-
-$utype=$_SESSION["usertype"];//user type assigned to session global variable
-$_SESSION["userstatus"] = $userstatus;//user stautus assigned to session global variable
-$_SESSION["userid"] = $userid;//user id assigned to session global variable
 include ("connect.php");//Establishing connection with our database
 //$dg = new C_DataGrid("SELECT * FROM users", "uid", "users"); This code is not functioning the way i want
-$sql='SELECT users.uid,users.userStatus,bugs.title,bugs.bugDesc FROM users,bugs  WHERE bugs.uid=$_SESSION["$uid"]';//select required dataset from database
+$sql="SELECT uid,userType,userStatus,username FROM users WHERE userStatus='0'";//select required dataset from database
 $result=mysqli_query($db,$sql);//fetch data from database
 
-echo '<h3>Unfixed Bugs</h3>';
+echo '<h3>Inactive Users Pending Activation</h3>';
 echo '<table border="1" style="width:60%">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'User ID'.
-    '</th>'.'<th>'.'Bug Title'.'</th>'.'<th>'.'Bug Descrition'.
+    '</th>'.'<th>'.'User Name'.'</th>'.'<th>'.'User Status'.
 
-    '</th>'.'<th>'.'Fixed Bug?'.'</th>'.'</table>';
+    '</th>'.'<th>'.'Activate'.'</th>'.'</table>';
 //loop through the database and fetch all users with userStatus=0
 WHILE($row=mysqli_fetch_assoc($result))
 {
     //get the userid, userTpe,userStatus,username
-    $uid=$row['users.uid'];
-    // $userType=$row['userType'];
-    $btitle=$row['bugs.title'];
-    $ustatus=$row['users.userStatus'];
-    $bdesc=$row['bugs.bugDesc'];
-
+    $uid=$row['uid'];
+    $userType=$row['userType'];
+    $userStatus=$row['userStatus'];
+    $username=$row['username'];
 
     echo '<table border="1" style="width:60%">'.'<col width="60">'. '<col width="60">'.'<col width="60">'.'<col width="60">'.'<tr>'.
-        '<a href="changeBugStatus.php?uid="'.$uid.'>'.'<tr>'.'<td>'.$uid.'</td>'.'<td>' . $btitle.'</td>'.'<td>'.
-        $bdesc.'</td>'.'<td>'."<input type='submit' name='submit' value = 'Activate'>".'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+        '<a href="inactive.php?uid="'.$uid.'>'.'<tr>'.'<td>'.$uid.'</td>'.'<td>' .$username.'</td>'.'<td>'.
+        $userType.'</td>'.'<td>'."<input type='submit' name='submit' value = 'Activate'>".'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
 }
 ?>
