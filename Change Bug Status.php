5c9855c4f6a5092c71d78868c8005d72cc998d18
1400205 <1400205@rgu.ac.uk>
2016-03-22 05:47:20 +0000

    html home page

---
+++ Change Bug Status.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Change Bug Status</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 15/03/2016
+ * Time: 08:21
+ *
+ *
+ */
+
+//get user ID in session
+
+$utype=$_SESSION["usertype"];//user type assigned to session global variable
+$_SESSION["userstatus"] = $userstatus;//user stautus assigned to session global variable
+$_SESSION["userid"] = $userid;//user id assigned to session global variable
+include ("connect.php");//Establishing connection with our database
+//$dg = new C_DataGrid("SELECT * FROM users", "uid", "users"); This code is not functioning the way i want
+$sql='SELECT users.uid,users.userStatus,bugs.title,bugs.bugDesc FROM users,bugs  WHERE bugs.uid=$_SESSION["$uid"]';//select required dataset from database
+$result=mysqli_query($db,$sql);//fetch data from database
+
+echo '<h3>Inactive Users Pending Activation</h3>';
+echo '<table border="1" style="width:60%">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'User ID'.
+    '</th>'.'<th>'.'Bug Title'.'</th>'.'<th>'.'Bug Descrition'.
+
+    '</th>'.'<th>'.'Fixed Bug?'.'</th>'.'</table>';
+//loop through the database and fetch all users with userStatus=0
+WHILE($row=mysqli_fetch_assoc($result))
+{
+    //get the userid, userTpe,userStatus,username
+    $uid=$row['users.uid'];
+  // $userType=$row['userType'];
+    $btitle=$row['bugs.title'];
+    $ustatus=$row['users.userStatus'];
+    $bdesc=$row['bugs.bugDesc'];
+
+
+    echo '<table border="1" style="width:60%">'.'<col width="60">'. '<col width="60">'.'<col width="60">'.'<col width="60">'.'<tr>'.
+        '<a href="inactive.php?uid="'.$uid.'>'.'<tr>'.'<td>'.$uid.'</td>'.'<td>' .$username.'</td>'.'<td>'.
+        $userType.'</td>'.'<td>'."<input type='submit' name='submit' value = 'Activate'>".'</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+}
+?>
