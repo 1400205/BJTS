5c7fe705686a00f3459e9dd34c8bc9b04f4e5fa1
1400205 <1400205@rgu.ac.uk>
2016-03-23 07:22:55 +0000

    try check box

--- adminChangeBugStatus.php
+++ adminChangeBugStatus.php
@@ -55,7 +55,7 @@ session_start();
 
     if(isset($_POST['submit'])){//to run PHP script on submit
         if(!empty($_POST['bugid'])){
-            $fixedDate=$_POST["fixedDate"];
+            $fixedDate=date();
 // Loop to store and display values of individual checked checkbox.
             foreach($_POST['bugid'] as $bugid)
             {
