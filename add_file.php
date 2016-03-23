3aeb0f72614caea4ef5f353f08e18c16d827508b
1400205 <1400205@rgu.ac.uk>
2016-03-23 19:30:06 +0000

    attachment

--- add_file.php
+++ add_file.php
@@ -10,6 +10,50 @@ session_start();
  */
 $uid=$_SESSION["userid"];
 ?>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: prosper
+ * Date: 15/03/2016
+ * Time: 08:21
+ *
+ *
+ */
+
+include ("connect.php");//Establishing connection with our database
+//$dg = new C_DataGrid("SELECT * FROM users", "uid", "users"); This code is not functioning the way i want
+$sql="SELECT bugID,title,bugDesc FROM bugs";//select required dataset from database
+//"SELECT bugID,title,bugDesc FROM bugs WHERE uid=1";//select required dataset from database
+$result=mysqli_query($db,$sql);//fetch data from database
+
+echo '<h3>Comment on bugs </h3>'.$_SESSION["$userid"];
+echo '<table border="1" style="width:60%">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'Bug ID'.
+    '</th>'.'<th>'.'Title'.'</th>'.'<th>'.'Description'.
+
+    '</th>'.'<th>'.'Select Bug'.'</th>'.'</table>';
+
+//loop through the database and fetch all users with userStatus=0
+WHILE($row=mysqli_fetch_assoc($result))
+{
+    //get the userid, userTpe,userStatus,username
+    $bugid=$row['bugID'];
+    $title=$row['title'];
+    $bugdesc=$row['bugDesc'];
+    // $username=$row['username];
+
+    echo '<table border="1" style="width:60%">'.'<col width="60">'. '<col width="60">'.'<col width="60">'.'<col width="60">'.'<tr>'.
+        '<a href="changeBugStatus.php?uid="'.$bugid.'>'.'<tr>'.'<td>'.$bugid.'</td>'.'<td>' . $title.'</td>'.'<td>'.
+        $bugdesc.'</td>'.'<td>'.
+        "<input type='radio' name='commentRadio' value='$bugid'>".
+        '</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+
+
+
+}
+//echo '<a href="commen">';
+?>
+
 <?php
 
 
