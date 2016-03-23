41f03c573208c0a28ee9ff373aea0b668e8bd17d
1400205 <1400205@rgu.ac.uk>
2016-03-23 09:25:14 +0000

    try check box

--- comments.php
+++ comments.php
@@ -58,7 +58,8 @@ session_start();
     }
     //echo '<a href="commen">';
     ?>
-    <textarea rows="5" cols="60" value="enter your comment here"> <br>
+    <textarea rows="5" cols="60" value="enter your comment here">
+        <p></p>
     <input type="submit" name="submit" value="submit">
     <?php
 
