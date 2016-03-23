c408ecc49a5970d2b905ab4508e5e96e1fe67813
1400205 <1400205@rgu.ac.uk>
2016-03-23 15:21:10 +0000

    attachment

--- upload.php
+++ upload.php
@@ -18,7 +18,8 @@ $uid=$_SESSION["userid"];
     <title>Admin Change Bug Fixed-Status</title>
 </head>
 <body>
-<form method="post" action="comments.php">
+
+    <form method="post" enctype="multipart/form-data"  action="upload.php">
     <?php
     /**
      * Created by PhpStorm.
@@ -92,6 +93,18 @@ $uid=$_SESSION["userid"];
         mysqli_close($db);
     }
     ?>
+
+
+        <table width="350" border="0" cellpadding="1" cellspacing="1" class="box">
+            <tr>
+                <td width="246">
+                    <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+                    <input name="userfile" type="file" id="userfile">
+                </td>
+                <td width="80"><input name="upload" type="submit" class="box" id="upload" value=" Upload "></td>
+            </tr>
+        </table>
+
 
 
 </form>
