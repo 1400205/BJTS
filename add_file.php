b364189d812b089083e93c52d028192280e4a808
1400205 <1400205@rgu.ac.uk>
2016-03-23 18:19:09 +0000

    attachment

--- add_file.php
+++ add_file.php
@@ -1,14 +1,14 @@
 <?php
+
+//connection string
 include("connect.php");
+
+
 // Check if a file has been uploaded
 if(isset($_FILES['uploaded_file'])) {
+
     // Make sure the file was sent without errors
     if($_FILES['uploaded_file']['error'] == 0) {
-        // Connect to the database
-        //$dbLink = new mysqli('127.0.0.1', 'user', 'pwd', 'myTable');
-       // if(mysqli_connect_errno()) {
-          //  die("MySQL connection failed: ". mysqli_connect_error());
-       // }
 
         // Gather all required data
         $name = $_FILES['uploaded_file']['name'];
@@ -17,23 +17,15 @@ if(isset($_FILES['uploaded_file'])) {
         $size = intval($_FILES['uploaded_file']['size']);
 
         // Create the SQL query
-       // $sql = "INSERT INTO file (name,mime, size, data, created VALUES ('$name', '$mime', '$size', '$data')";
-         echo fname."<br>";
-        echo $mime."<br>";
-        echo $size."<br>";
-        echo $data."<br>";
-
         $qry="INSERT  INTO myfile(fname, fmime, fsize,fdata) VALUES ('$name', '$mime','$size','$data')";
 
         // Execute the query
-       // $result = $dbLink->query($query);
-
         // Check if it was successfull
         if (mysqli_query($db, $qry)){
             echo 'Success! Your file was successfully added!';
         }
         else {
-            echo "ERROR: Could not be able to execute".$qry. mysqli_error($db);
+            echo "ERROR: Could not be able to execute";//.$qry. mysqli_error($db);
         }
     }
     else {
