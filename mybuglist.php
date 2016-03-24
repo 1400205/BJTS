77ed71e10518ca4a98cb2a8321a9acac13422ef9
1400205 <1400205@rgu.ac.uk>
2016-03-24 10:13:47 +0000

    attachment

---
+++ mybuglist.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Change Bug Fix Status</title>
+    <link rel="stylesheet"type="text/css" href="style.css"/>
+</head>
+<body>
+<form method="post" action="changeBugStatus.php">
+    <fieldset>
+        <legend>Change Bug Fixed-Status</legend>
+        <?php
+        /**
+         * Created by PhpStorm.
+         * User: prosper
+         * Date: 15/03/2016
+         * Time: 08:21
+         *
+         *
+         */
+
+        include ("connect.php");//Establishing connection with our database
+
+        //$dg = new C_DataGrid("SELECT * FROM users", "uid", "users"); This code is not functioning the way i want
+        $sql="SELECT bugID,title,bugDesc FROM bugs";//select required dataset from database
+
+        //get the result
+        $result=mysqli_query($db,$sql);//fetch data from database
+
+        echo '<table border="1" style="width:60%">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<col width="60">'.'<th>'.'Bug ID'.
+            '</th>'.'<th>'.'Title'.'</th>'.'<th>'.'Description'.
+
+            '</th>'.'<th>'.'Activate'.'</th>'.'</table>';
+        //loop through the database and fetch all users with userStatus=0
+        WHILE($row=mysqli_fetch_assoc($result))
+        {
+            //get the userid, userTpe,userStatus,username
+            $bugid=$row['bugID'];
+            $title=$row['title'];
+            $bugdesc=$row['bugDesc'];
+            // $username=$row['username'];
+
+            echo '<table border="1" style="width:60%">'.'<col width="60">'. '<col width="60">'.'<col width="60">'.'<col width="60">'.'<tr>'.
+                '<a href="changeBugStatus.php?uid="'.$bugid.'>'.'<tr>'.'<td>'.$bugid.'</td>'.'<td>' . $title.'</td>'.'<td>'.
+                $bugdesc.'</td>'.'<td>'.
+                "<input type='checkbox' name='bugid[]' value = '$bugid'>".
+                '</td>'.'</a>'.'<br>'.'</tr>'.'</table>';
+
+
+
+        }
+        ?>
+
+    </fieldset>
+
+</form>
+
+</body>
