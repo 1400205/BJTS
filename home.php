0c80d2aed2cc23df514fc7439f75f4959125b148
1400205 <1400205@rgu.ac.uk>
2016-03-24 17:00:06 +0000

    attachment

--- home.php
+++ home.php
@@ -10,8 +10,53 @@ session_start();
 </head>
 
 <body>
-<h1>Hello</h1>
+<<form>
 
+
+    <fieldset>
+        <legend><H2>WELCOME</H2></legend>
+        <div id="container">
+
+            <div id="nav">
+
+                <h3><marquee behavior="alternate">
+                        WLECOME TO BJTS
+                    </marquee>
+                    <p>  <?php
+                        echo $uid;
+                        ?></p>
+                </h3><br>
+                <h4>Add Records</h4>
+                <ul>
+                    <a href="activateUsers.php">  <li>Activate Users</li></a>
+                    <a href="bugRegister.php">  <li>Add Bug</li></a>
+                    <a href="comments.php">  <li>Comment on Bugs</li></a>
+                    <a href="adminChangeBugStatus.php">  <li>Admin Approve Bug Changes</li></a>
+                    <a href="changeBugStatus.php">  <li>User Change Bug Status</li></a>
+                    <a href="upload.php">  <li>Attached File to Bug</li></a>
+                </ul>
+                <h4>View All</h4>
+                <ul>
+                    <a href="mybuglist.php">  <li>View All Bugs</li></a>
+                    <a href="buglist.php">  <li>View Fixed Bugs</li></a>
+                    <a href="">  <li>View Unfixed Bugs</li></a>
+                    <a href="">  <li>Bugs With Comments</li></a>
+                    <a href="">  <li>View Bugs Pending Approval</li></a>
+
+                </ul>
+            </div>
+            <div id="main">
+
+
+            </div>
+            <dev id="footer">
+
+
+            </dev>
+        </div>
+        </div>
+    </fieldset>
+</form>
 <?php
 // Echo session variables that were set on previous page
 echo "user name: " . $_SESSION["uname"] . ".<br>";
--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if(mysqli_num_rows($result) == 1  and $userstatus==1) {
 
     $usertype = $row['userType'];//get user type
     $userstatus = $row['userStatus'];//get user status
-    header("location: Home.html"); // Redirecting To another Page
+    header("location: home.php"); // Redirecting To another Page
 
     $_SESSION["uname"] = $username;
     $_SESSION["pwd"] = $password;
