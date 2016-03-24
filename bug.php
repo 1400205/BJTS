c683900f85870c89af62870abc48aadb3a344702
1400205 <1400205@rgu.ac.uk>
2016-03-24 09:59:52 +0000

    attachment

--- bug.php
+++ bug.php
@@ -1,65 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bug and Job Tracking System</title>
+</head>
+<body>
 
-<?php
-//open start session
+<div id="content">
+    <?php
+    //Establishing connection with database
+    include("connect.php");
 
-session_start();
-?>
+    //select everything from bug table
+    $sql="SELECT * FROM bugs WHERE bugID=".$_GET["ID"];
 
-<?php
-/**
- * Created by PhpStorm.
- * User: prosper
- * Date: 21/03/2016
- * Time: 15:39
- */
+    //fetch result from database
+    $result=mysqli_query($db,$sql);
 
-//call connection string fucntion
-include ("connect.php");//Establishing connection with our database
-
-if ((empty($_POST["title"])) OR (empty($_POST["description"])))
-{
-    echo "Please all fields are required";
-}
-
-elseif(isset($_POST["submit"])) {
-    $title = $_POST["title"];
-    $description = $_POST["description"];
-    $uid = $_SESSION["userid"];
-
-//strip variables of all sql injections
-    $title = mysqli_real_escape_string($db, $title);
-    $description = mysqli_real_escape_string($db, $description);
-    $uid = mysqli_real_escape_string($db, $uid);
-
-    $sql = "SELECT title FROM bugs WHERE title='$title'";
-    $result = mysqli_query($db, $sql);
-    $row = mysqli_fetch_array($result,mysqli_fetch_assoc);//get the row of data
-   // echo $row["title"];
-    if( (mysqli_num_rows($row))<1  )
-    {
-       // echo 'add records';
-        $qry="INSERT INTO bugs(uid,title,bugDesc)VALUES('$uid','$title','$description')";
-
-        if(mysqli_query($db, $qry)){
-            //$_SESSION['success']= "Records added successfully.";
-
-            //redirect user to login screen
-           // header("location: index.php");
-            echo "Record successfully Submitted";
-        } else{
-            echo "ERROR: Could not be able to execute. Check duplicate record entry"/**$qry. mysqli_error($db)*/;
-        }
-
-// Close connection
-        mysqli_close($db);
+    //scan through each row in the response
+    $row=mysqli_fetch_assoc($result);
+        //get title and id from the bug
+        $bugTitle=$row['title'];
+        $bugID=$row['bugID'];
+    $bugDesc=$row['bugDesc'];
+        //Write the link to the page
+        echo "<h2>".$bugTitle."</h2>";
+    echo "<p2>".$bugDesc."</p2>";
 
 
-    }
 
 
-}elseif( (mysqli_num_rows($row)==1) ||( mysqli_num_rows($row>1) ))
-{
-   $errormsg="Record Already Exist. Duplicate Bug entry is Not Allowed";
-    echo  "$errormsg";
+    ?>
 
-}
+</div>
+
+
+
+</body>
+</html>
